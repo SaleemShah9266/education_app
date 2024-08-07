83b5a6179efe81e3b3a4e77c4300028e0e30e7b9
Saleem Shah <saluu1212@gmail.com>
2024-08-07 21:57:30 +0500

    data and model added

--- lib/Screens/WelcomeScreen.dart
+++ lib/Screens/WelcomeScreen.dart
@@ -24,7 +24,14 @@ class WelcomeScreen extends StatelessWidget {
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height / 1.6,
                   decoration: BoxDecoration(
-                    color: Colors.red,
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.indigo,
+                        Colors.purple,
+                      ],
+                    ),
                     borderRadius:
                         BorderRadius.only(bottomRight: Radius.circular(70)),
                   ),
@@ -36,6 +43,76 @@ class WelcomeScreen extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height / 2.666,
+                padding: EdgeInsets.only(top: 4, bottom: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(70),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "Learning is Everything",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        "Learning with Saleem, Wherever you are!",
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.black.withOpacity(0.6),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 80),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.indigo,
+                            Colors.purple,
+                          ],
+                        ),
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Get Start",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ),
